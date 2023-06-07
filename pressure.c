a935c5ac3fda9723a6bd2aa2499111ada1533930
세현 임 <tpgus2604@ajou.ac.kr>
2023-06-07 13:25:45 +0900

    헤더파일로 분리된 압력센서

---
+++ pressure.c
@@ -0,0 +1,100 @@
+#include <fcntl.h>
+#include <getopt.h>
+#include <linux/spi/spidev.h>
+#include <linux/types.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "spi.h"
+
+#define IN 1
+#define OUT 0
+#define LOW 0
+#define HIGH 1
+#define VALUE_MAX 256
+
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof((a)[0]))
+
+static const char *DEVICE = "/dev/spidev0.0";
+static uint8_t MODE = 0;
+static uint8_t BITS = 8;
+static uint32_t CLOCK = 1000000;
+static uint16_t DELAY = 5;
+
+static int prepare(int fd){
+    if(ioctl(fd,SPI_IOC_WR_MODE, &MODE) == -1){
+        perror("Can't set MODE");
+        return -1;
+    }
+
+    if(ioctl(fd,SPI_IOC_WR_BITS_PER_WORD, &BITS) == -1){
+        perror("Can't set number of BITS");
+        return -1;
+    }
+
+    if(ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ,&CLOCK)==-1){
+        perror("Can't set write CLOCK");
+        return -1;
+    }
+
+    if(ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &CLOCK)==-1){
+        perror("Can't set read CLOCK");
+        return -1;
+    }
+
+    return 0;
+}
+
+uint8_t control_bits_differential(uint8_t channel){
+    return (channel & 7) << 4;
+}
+
+uint8_t control_bits(uint8_t channel){
+    return 0x8 | control_bits_differential(channel);
+}
+
+int readadc(int fd, uint8_t channel){
+    uint8_t tx[] = {1, control_bits(channel),0};
+    uint8_t rx[3];
+
+    struct spi_ioc_transfer tr = {
+        .tx_buf = (unsigned long)tx,
+        .rx_buf = (unsigned long)rx,
+        .len = ARRAY_SIZE(tx),
+        .delay_usecs = DELAY,
+        .speed_hz = CLOCK,
+        .bits_per_word = BITS,
+    };
+
+    if(ioctl(fd, SPI_IOC_MESSAGE(1), &tr) == 1){
+        perror("IO Error");
+        abort();
+    }
+
+    return((rx[1] << 8) & 0x300) | (rx[2] & 0xFF);
+}
+
+int pressure_sensor( ){
+    int result = 0;
+    int fd = open(DEVICE, O_RDWR);
+    if(fd <= 0){
+        perror("Device open error");
+        return -1;
+    }
+
+    if(perpare(fd) == -1){
+        perror("Device prepare error");
+        return -1;
+    }
+
+        result = readadc(fd,0);
+        return result;
+    
+}
+    
+
