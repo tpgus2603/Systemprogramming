11c86ebacc61fde3e88e5f94dac7d151c0ebe145
owl2lwo12 <owl2lwo12@ajou.ac.kr>
2023-06-09 20:15:47 +0900

    hh

--- actclientsmpl.c
+++ actclientsmpl.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include <unistd.h>
 #include <string.h>
@@ -22,8 +23,11 @@
 #define LOW 0
 #define HIGH 1
 
+#define VALUE_MAX 45
+#define DIRECTION_MAX 45
+
 // GPIO
-#define BUZ 18
+#define BUZ 0
 #define LED1 17
 #define LED2 27
 #define LED3 22
@@ -64,6 +68,106 @@ void error_handling(char* message) {
     exit(1);
 }
 
+static int PWMExport(int pwmnum) {
+#define BUFFER_MAX 3
+    char buffer[BUFFER_MAX];
+    int bytes_written;
+    int fd;
+
+    fd = open("/sys/class/pwm/pwmchip0/unexport", O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open in unexport!\n");
+        return(-1);
+    }
+
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pwmnum);
+    write(fd, buffer, bytes_written);
+    close(fd);
+
+    sleep(1);
+    fd = open("/sys/class/pwm/pwmchip0/export", O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open in export!\n");
+        return(-1);
+    }
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pwmnum);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    sleep(1);
+    return(0);
+}
+
+static int PWMEnable(int pwmnum) {
+    static const char s_unenable_str[] = "0";
+    static const char s_enable_str[] = "1";
+    char path[DIRECTION_MAX];
+    int fd;
+
+    snprintf(path, DIRECTION_MAX, "/sys/class/pwm/pwmchip0/pwm%d/enable", pwmnum);
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open in enable!\n");
+        return -1;
+    }
+
+    write(fd, s_unenable_str, strlen(s_unenable_str));
+    close(fd);
+
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open in enable!\n");
+        return -1;
+    }
+
+    write(fd, s_enable_str, strlen(s_enable_str));
+    close(fd);
+    return(0);
+}
+
+static int PWMWritePeriod(int pwmnum, int value) {
+    char s_values_str[VALUE_MAX];
+    char path[VALUE_MAX];
+    int fd, byte;
+
+    snprintf(path, VALUE_MAX, "/sys/class/pwm/pwmchip0/pwm%d/period", pwmnum);
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open in period!\n");
+        return(-1);
+    }
+    byte = snprintf(s_values_str, VALUE_MAX, "%d", value);
+    if (-1 == write(fd, s_values_str, byte)) {
+        fprintf(stderr, "Failed to write value in period!\n");
+        close(fd);
+        return(-1);
+    }
+    close(fd);
+    return(0);
+}
+static int PWMWriteDutyCycle(int pwmnum, int value) {
+    char path[VALUE_MAX];
+    char s_values_str[VALUE_MAX];
+    int fd, byte;
+
+    snprintf(path, VALUE_MAX, "/sys/class/pwm/pwmchip0/pwm%d/duty_cycle", pwmnum);
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open in duty_cycle!\n");
+        return(-1);
+    }
+
+    byte = snprintf(s_values_str, VALUE_MAX, "%d", value);
+
+    if (-1 == write(fd, s_values_str, byte)) {
+        fprintf(stderr, "Failed to write value! in duty_cycle\n");
+        close(fd);
+        return(-1);
+    }
+
+    close(fd);
+    return(0);
+}
+
 int main(int argc, char* argv[]) {
     /*int sock;
     struct sockaddr_in serv_addr;
@@ -82,6 +186,11 @@ int main(int argc, char* argv[]) {
     printf("fd = %d \n", fd);
 
     lcd_init(); // setup LCD
+
+    PWMExport(BUZ);
+    PWMWritePeriod(BUZ, 2000000); // lower number, higher sound!!
+    PWMWriteDutyCycle(BUZ, 0);
+    PWMEnable(0);
 
     /*wiringPiSetupGpio();
     pinMode(BUZ, PWM_OUTPUT);
@@ -103,7 +212,7 @@ int main(int argc, char* argv[]) {
         typeln("Using wiringPi");
         lcdLoc(LINE2);
         typeln("kjpark editor.");
-
+        PWMWriteDutyCycle(BUZ, 10000);
 
         /*pwmWrite(BUZ, 1000000 / 262 / 2);
         delay(3000);*/
--- buzzer.c
+++ buzzer.c
@@ -1,49 +1,138 @@
-
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <wiringPi.h>
-#define buzzer_pin 18
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "buzzer.h"
 
-int main(void) 
-{
+#define VALUE_MAX 45
+#define DIRECTION_MAX 45
+#define buz 0
 
-    wiringPiSetupGpio();
-    pinMode(buzzer_pin, PWM_OUTPUT);
-
-    pwmSetClock(19);
-    pwmSetMode(PWM_MODE_MS);
-
-    pwmSetRange(1000000/262);
-    pwmWrite(buzzer_pin, 1000000/262/2);
-    delay(3000);
-
-    pwmWrite(buzzer_pin,0);
+static int PWMExport(int pwmnum){
+   #define BUFFER_MAX 3
+   char buffer[BUFFER_MAX];
+   int bytes_written;
+   int fd;
+   
+   fd = open("/sys/class/pwm/pwmchip0/unexport",O_WRONLY);
+   if(-1==fd){
+      fprintf(stderr, "Failed to open in unexport!\n");
+      return(-1);
+   }
+   
+   bytes_written = snprintf(buffer, BUFFER_MAX,"%d",pwmnum);
+   write(fd,buffer,bytes_written);
+   close(fd);
+   
+   sleep(1);
+   fd = open("/sys/class/pwm/pwmchip0/export",O_WRONLY);
+   if(-1==fd){
+      fprintf(stderr,"Failed to open in export!\n");
+      return(-1);
+   }
+   bytes_written = snprintf(buffer, BUFFER_MAX,"%d",pwmnum);
+   write(fd,buffer,bytes_written);
+   close(fd);
+   sleep(1);
+   return(0);
 }
 
-// softTone.h를 사용한 코드
+static int PWMEnable(int pwmnum){
+   static const char s_unenable_str[] = "0";
+   static const char s_enable_str[] = "1";
+   char path[DIRECTION_MAX];
+   int fd;
+   
+   snprintf(path, DIRECTION_MAX, "/sys/class/pwm/pwmchip0/pwm%d/enable",pwmnum);
+   fd = open(path,O_WRONLY);
+   if(-1==fd){
+      fprintf(stderr,"Failed to open in enable!\n");
+      return -1;
+   }
+   
+   write(fd,s_unenable_str,strlen(s_unenable_str));
+   close(fd);
+   
+   fd=open(path,O_WRONLY);
+   if(-1==fd){
+      fprintf(stderr,"Failed to open in enable!\n");
+      return -1;
+   }
+   
+   write(fd,s_enable_str,strlen(s_enable_str));
+   close(fd);
+   return(0);
+}
 
-// #include <stdio.h>
-// #include <wiringPi.h>
-// #include <softTone.h>
+static int PWMWritePeriod(int pwmnum, int value){
+   char s_values_str[VALUE_MAX];
+   char path[VALUE_MAX];
+   int fd,byte;
+   
+   snprintf(path, VALUE_MAX, "/sys/class/pwm/pwmchip0/pwm%d/period",pwmnum);
+   fd = open(path,O_WRONLY);
+   if(-1==fd){
+      fprintf(stderr, "Failed to open in period!\n");
+      return(-1);
+   }
+   byte=snprintf(s_values_str,VALUE_MAX,"%d",value);
+   if(-1==write(fd,s_values_str,byte)){
+      fprintf(stderr,"Failed to write value in period!\n");
+      close(fd);
+      return(-1);
+   }
+   close(fd);
+   return(0);
+}
+static int PWMWriteDutyCycle(int pwmnum, int value){
+   char path[VALUE_MAX];
+   char s_values_str[VALUE_MAX];
+   int fd,byte;
+   
+   snprintf(path, VALUE_MAX, "/sys/class/pwm/pwmchip0/pwm%d/duty_cycle",pwmnum);
+   fd = open(path, O_WRONLY);
+   if(-1==fd){
+      fprintf(stderr, "Failed to open in duty_cycle!\n");
+      return(-1);
+   }
+   
+   byte = snprintf(s_values_str,VALUE_MAX,"%d",value);
+   
+   if(-1==write(fd,s_values_str,byte)){
+      fprintf(stderr, "Failed to write value! in duty_cycle\n");
+      close(fd);
+      return(-1);
+   }
+   
+   close(fd);
+   return(0);
+}
 
-// const int pinPiezo = 18;
+void buzzerHigh(int pin1){
+   PWMExport(buz);
+   PWMWritePeriod(buz, 2000000); // lower number, higher sound!!
+   PWMWriteDutyCycle(buz,0);
+   PWMEnable(0);
+   int i=0;
+    while(1){
+        PWMWriteDutyCycle(buz,10000);
+    }
+}
+void buzzerLow(int pin1){
+    PWMExport(buz);
+   PWMWritePeriod(buz, 2000000); // lower number, higher sound!!
+   PWMWriteDutyCycle(buz,0);
+   PWMEnable(0);
+   int i=0;
+    while(1){
+        PWMWriteDutyCycle(buz,10000);
+    }
 
-// const int aMelody[8] = {131,147,165,175,196,220,247,262};
-
-// int main(void)
-// {
-// 	wiringPiSetupGpio();
-
-// 	softToneCreate(pinPiezo);
-
-// 	for (i = 0 ; i < 8 ; ++i)
-
-//     {
-
-//       printf ("%3d\n", i) ;
-
-//       softToneWrite (pinPiezo, aMelody[i]) ;
-
-//       delay (500) ;
-
-//     }
-// }
+}
