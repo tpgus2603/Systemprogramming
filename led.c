01969ca2aa2ad5029d2a692546644677cd95f324
세현 임 <tpgus2604@ajou.ac.kr>
2023-06-22 19:28:04 +0900

    led5초간 불 들어오는 코드

---
+++ led.c
@@ -0,0 +1,16 @@
+#include"gpio.h"
+#include"led.h"
+int ledOn(int pin) {
+    if (GPIOExport(pin) == -1)
+        return(-1);
+    usleep(1000 * 10);
+    if (GPIODirection(pin, 1) == -1)
+        return(-1);
+    if (GPIOWrite(pin, 1) == -1)
+        return(-1);
+    usleep(5000 * 1000);
+    if (GPIOWrite(pin, 0) == -1)
+        return(-1);
+    if (GPIOUnexpot(pin) == -1)
+        return(-1);
+}
